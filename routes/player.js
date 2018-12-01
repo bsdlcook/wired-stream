94b07d3bb123e67e6a57b7c592247e9f035d2002
syne <syne@wired.sh>
2018-12-01 20:30:49 +0000

    Now using partial views and updated some code

--- routes/player.js
+++ routes/player.js
@@ -4,9 +4,6 @@ import mime from 'mime-types';
 const player = express.Router();
 export default class WiredPlayer {
   playerRouter() {
-    player.get('/', (req, res) => {
-      this.randomFile(res);
-    });
     player.get('/:id', (req, res) => {
       const id = req.params.id;
       if (!this.findFile(id)) return this.randomFile(res);
@@ -14,17 +11,19 @@ export default class WiredPlayer {
         return res.status(200).redirect('/' + this.playerUrl + '/' + id);
       const file = this.findFile(id);
       res.status(200).render('player', {
-        layout: 'main',
         title: this.cleanName(file),
         file_name: this.cleanName(file),
         file_id: this.findIndex(file),
         file_count: this.fileCount(),
         file_mime: mime.lookup(this.filePath + file),
         file_hash: this.genHash(file),
-	url: req.get('host'),
-	api_url: this.apiUrl,
-        player_url: this.playerUrl
+        url: req.get('host'),
+        api_url: this.apiUrl,
+        player_url: this.playerUrl,
       });
+    });
+    player.get('/', (req, res) => {
+      this.randomFile(res);
     });
     return player;
   }
